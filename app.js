dea50ee9b8c0ce94b21b035a31f31f7bc2f9228e
nilsgeiselhart <nigeiselh@gmail.com>
2018-02-24 16:22:19 +0100

    lcnse

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 /**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+Based on the example: https://github.com/spotify/web-api-auth-examples.
+Modifications (c) 2018 me
  */
 
 const fs = require('fs');
